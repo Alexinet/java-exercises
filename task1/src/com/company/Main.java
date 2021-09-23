854b81b47c4c1f3f80af961793c1d79c53f78441
Alexinet <88674225+Alexinet@users.noreply.github.com>
2021-09-23 13:29:53 +0300

    task1(complete) + gitignore

--- task1/src/com/company/Main.java
+++ task1/src/com/company/Main.java
@@ -10,27 +10,58 @@ public class Main {
         for (int i = 2; i < 12; i++) {
             System.out.println("1/" + (i * i - 1));
         }
+        System.out.println("\n");
+    }
+
+    private static void swap(int[] array, int ind1, int ind2) {
+        int tmp = array[ind1];
+        array[ind1] = array[ind2];
+        array[ind2] = tmp;
     }
 
     public static void func_six() {
-        Random masQuantity = new Random();
-        masQuantity.nextInt(100);
-
+        Random random = new Random();
+        int masQuantity = random.nextInt(100);
         int[] arrayRandom = new int[masQuantity] ;
 
         for (int i = 0; i < masQuantity; i++) {
             arrayRandom[i] = (int) Math.round(100 * Math.random());
         }
 
+        System.out.println("Вывод неотсоритрованного массива:\n" + Arrays.toString(arrayRandom));
 
-        System.out.println(masQuantity);
-        System.out.println(Arrays.toString(arrayRandom));
+        boolean needIteration = true;
+        while (needIteration) {
+            needIteration = false;
+            for (int i = 1; i < arrayRandom.length; i++) {
+                if (arrayRandom[i] < arrayRandom[i - 1]) {
+                    swap(arrayRandom, i, i-1);
+                    needIteration = true;
+                }
+            }
+        }
+        System.out.println("\nВывод отсортированного массива:\n" + Arrays.toString(arrayRandom));
+    }
 
+    private static int factorial(int num) {
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+
+    public static void func_seven() {
+        Random random = new Random();
+        int number = random.nextInt(10);
+
+        System.out.println("\nЧисло равно " + number);
+        System.out.println("\nФакториал числа равен " + factorial(number));
     }
 
     public static void main(String[] args) {
-
-        //func_five();
+        func_five();
         func_six();
+        func_seven();
     }
 }
